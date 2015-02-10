711d3269b56478802ffa5ebfea285449aebbef3a
Matt Delhey <matt.delhey@gmail.com>
2015-02-09 22:27:08 -0600

    fix mixture model sampling (previously was not stochastic) and tests are working now

--- R/model-mixture-beta.R
+++ R/model-mixture-beta.R
@@ -1,35 +1,40 @@
-model.mixture.beta <- function(x, alpha1) {
-    data <- model.normal.summary(x)
-    return(posterior.parameters)
-}
-
-model.mixture.beta.importance <- function(n.samples, h, alpha.1, beta.1, delta.1, alpha.2, beta.2, delta.2, ...) {
-    if (n.samples %% 1 != 0) stop("number of samples must be an integer")
+model.mixture.beta.importance <- function(n.samples, h, alpha.1, beta.1, alpha.2, beta.2, delta, ...) {
     if (!is.function(h)) stop("h must be a function (use identity for expectation)")
 
     #x <- rbeta(n.samples, 1, 1)
-    #weights <- dbetamixture(x, alpha.1, beta.1, delta.1, alpha.2, beta.2, delta.2) / dbeta(x, 1, 1)
+    #weights <- dbetamixture(x, alpha.1, beta.1, alpha.2, beta.2, delta) / dbeta(x, 1, 1)
 
     x <- runif(n.samples, 0, 1)
-    weights <- dbetamixture(x, alpha.1, beta.1, delta.1, alpha.2, beta.2, delta.2) / dunif(x, 0, 1)
-    
+    weights <- dbetamixture(x, alpha.1, beta.1, alpha.2, beta.2, delta) / dunif(x, 0, 1)
+
+    # Standardized weights
     weights.std <- weights / sum(weights)
-    expectation.h <- sum(h(x, ...) * weights.std)
+    expectation.h <- sum(h(x, ...) * weights.std)    
+
+    # Raw weights
+    expectation.h.raw <- sum(h(x, ...) * weights) / n.samples
+
+    if (!isTRUE(all.equal(expectation.h, expectation.h.raw, tolerance = 0.01)))
+        warning("Expectation differs for standardized and raw weights ")
+    
     return(expectation.h)
 }
 
-rbetamixture <- function(n, alpha.1, beta.1, delta.1, alpha.2, beta.2, delta.2) {
-    check.betamixture(alpha.1, beta.1, delta.1, alpha.2, beta.2, delta.2)
-    delta.1*rbeta(n, alpha.1, beta.1) + delta.2*rbeta(n, alpha.2, beta.2)   
+rbetamixture <- function(n, alpha.1, beta.1, alpha.2, beta.2, delta) {
+    check.betamixture(n, alpha.1, beta.1, alpha.2, beta.2, delta)
+    state.vec <- sample(1:2, prob = c(delta, 1 - delta), size = n.samples, replace = TRUE)    
+    rbeta(n = n.samples, shape1 = c(alpha.1, alpha.2)[state.vec], shape2 = c(beta.1, beta.2)[state.vec])
 }
 
-dbetamixture <- function(x, alpha.1, beta.1, delta.1, alpha.2, beta.2, delta.2) {
-    check.betamixture(alpha.1, beta.1, delta.1, alpha.2, beta.2, delta.2)
-    delta.1*dbeta(x, alpha.1, beta.1) + delta.2*dbeta(x, alpha.2, beta.2)   
+rbetamixtrue
+
+dbetamixture <- function(x, alpha.1, beta.1, alpha.2, beta.2, delta) {
+    check.betamixture(length(x), alpha.1, beta.1, alpha.2, beta.2, delta)
+    delta*dbeta(x, alpha.1, beta.1) + (1-delta)*dbeta(x, alpha.2, beta.2)   
 }
 
-check.betamixture <- function(alpha.1, beta.1, delta.1, alpha.2, beta.2, delta.2) {
-    stopifnot(delta.1 + delta.2 == 1 & alpha.1 > 0 & alpha.2 > 0 & beta.1 > 0 & beta.2 > 0)
+check.betamixture <- function(n, alpha.1, beta.1, alpha.2, beta.2, delta) {
+    stopifnot(delta >= 0 & delta <= 1 & alpha.1 > 0 & alpha.2 > 0 & beta.1 > 0 & beta.2 > 0 & n %% 1 == 0)
 }
 
 
--- tests/testthat/test-beta-mixture.R
+++ tests/testthat/test-beta-mixture.R
@@ -1,36 +1,29 @@
 alpha.1 <- 5
 beta.1  <- 2
-delta.1 <- 0.3
-
 alpha.2 <- 2
 beta.2  <- 8
-delta.2 <- 0.7
+delta   <- 0.3
 
 n.samples <- 100000
 
-mixture <- rbetamixture(
-    n.samples
-  , alpha.1 = alpha.1
-  , beta.1  = beta.1
-  , delta.1 = delta.1
-  , alpha.2 = alpha.2
-  , beta.2  = beta.2
-  , delta.2 = delta.2)
+mixture <- rbetamixture(n.samples, alpha.1, beta.1, alpha.2, beta.2, delta)
 hist(mixture, prob = TRUE)
 lines(density(mixture))
 
 square <- function(x) x^2
 square.minus <- function(x, mu) (x - mu)^2
+indicator <- function(x, lower, upper) ifelse(x >= lower & x <= upper, 1, 0)
 
 expectation.mu <- model.mixture.beta.importance(
     n.samples
   , h       = identity
   , alpha.1 = alpha.1
   , beta.1  = beta.1
-  , delta.1 = delta.1
   , alpha.2 = alpha.2
   , beta.2  = beta.2
-  , delta.2 = delta.2)
+  , delta   = delta)
+mean(mixture)
+expectation.mu
 
 expectation.sigma2 <- model.mixture.beta.importance(
     n.samples
@@ -38,31 +31,22 @@ expectation.sigma2 <- model.mixture.beta.importance(
   , mu      = expectation.mu
   , alpha.1 = alpha.1
   , beta.1  = beta.1
-  , delta.1 = delta.1
   , alpha.2 = alpha.2
   , beta.2  = beta.2
-  , delta.2 = delta.2)
+  , delta   = delta)
+var(mixture)
+expectation.sigma2
 
 expectation.square <- model.mixture.beta.importance(
     n.samples
-  , h       = sqrt
+  , h       = square
   , alpha.1 = alpha.1
   , beta.1  = beta.1
-  , delta.1 = delta.1
   , alpha.2 = alpha.2
   , beta.2  = beta.2
-  , delta.2 = delta.2)
-
-mean(mixture)
-expectation.mu
-
+  , delta   = delta)
 var(mixture)
-expectation.sigma2
 expectation.square - expectation.mu^2
-
-mean(mixture >= 0.45 & mixture <= 0.55)
-
-indicator <- function(x, lower, upper) if (x >= lower && x <= upper) x else 0
 
 expectation.prob <- model.mixture.beta.importance(
     n.samples
@@ -71,9 +55,8 @@ expectation.prob <- model.mixture.beta.importance(
   , upper   = 0.55
   , alpha.1 = alpha.1
   , beta.1  = beta.1
-  , delta.1 = delta.1
   , alpha.2 = alpha.2
   , beta.2  = beta.2
-  , delta.2 = delta.2)
-
+  , delta   = delta)
 expectation.prob
+mean(mixture >= 0.45 & mixture <= 0.55)
